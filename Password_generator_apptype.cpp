f8e72a3975e304459af4057981237405d6170f4c
Proytookh <protookhdutta1@gmail.com>
2023-05-20 11:46:18 +0530

    Do- while loop has been added along with boolean statements.. There was an error in which the password displayed and stored were not similar.. The error is corrected

--- Password_generator_apptype.cpp
+++ Password_generator_apptype.cpp
@@ -15,7 +15,7 @@ class File
         {
             string input=temp;
             string str_file;
-            cout<<"Enter name of the file: "<<endl;
+            cout<<"Enter name of the file (--filename.txt--): ";
             cin>>str_file;
             ofstream MyFile(str_file);
             MyFile<<temp<<endl;
@@ -32,7 +32,7 @@ class Random
         {
             char alphabet[MAX]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
                                 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-            string res=" ";
+            string res;
             for(int i=0;i<size;i++)
             {
                 res=res+alphabet[rand()% MAX];
@@ -81,13 +81,31 @@ class Random
     
     public:
         string final_conv(string res, int size){
-            string final_str=" ";
+            string final_str;
             for(int i=0;i<size;i++)
             {
-                final_str=final_str+res[rand()%(size*3)];
+                final_str=final_str+res[rand()%res.size()];
             }
             return final_str;
         }
+
+};
+
+class Check{
+    public:
+        bool istrue(char ch)
+        {
+            if(ch=='y' || ch=='Y')
+                return true;
+            else if(ch=='n' || ch=='N')
+                return false;
+            else
+            {
+                cout<<"\nEnter (y/n) in console!"<<endl;
+                return false;
+            }
+        }
+    
     public:
         bool check_case(int num)
         {
@@ -96,92 +114,138 @@ class Random
             else
                 return false;
         }
+    public:
+        bool ask(){
+            char c;
+            cout<<"\nDo you wish to create a txt file (y/n): ";
+            cin>>c;
+            if(c=='y' || c=='Y')
+                return true;
+            else if(c=='n' || c=='N')    
+                return false;
+            else
+            {
+                cout<<"\nEnter (y/n) in the console: "<<endl;
+                return false;
+            }
+        }
 };
 
 int main()
 {
     srand(time(NULL));
+    string generatedPassword;
     string str1,str2,str3;
     Random r;
     File f;
+    Check c;
     int size,ch;
-    char c;
+    char chc='y';
     
     cout<<"Welcome to password generator!"<<endl;
-    cout<<"The options are:\n1.String generator\n2.Number generator\n3.Character generator\n4.String, Character generator\n5.String, Number generator \n6.Character, Number generator\n7.String, Character, Number generator"<<endl;
-    
-    cout<<"Enter your option: "<<endl;
+    cout<<endl;
+    cout<<"The options are:"<<endl;
+    cout<<"1. String Generator"<<endl;
+    cout<<"2. Number generator"<<endl;
+    cout<<"3. Symbol generator"<<endl;
+    cout<<"4. String, Symbol generator"<<endl;
+    cout<<"5. String, Number generator"<<endl;
+    cout<<"6. Character, Number generator"<<endl;
+    cout<<"7. String, Character, Number generator"<<endl;
+
+
+    cout<<"Enter your option: ";
     cin>>ch;
 
-    if(r.check_case(ch)==true)
+    if(c.check_case(ch)==true)
     {
-        cout<<"Enter the size of the password: "<<endl;
+        cout<<"Enter the size of the password: ";
         cin>>size;
     }
     else
         cout<<"Case not valid";
 
     vector<int> randomNumbers=r.random_num(size);
-    str1=r.string_gen(size);
+    //str1=r.string_gen(size);
     //r.print_num(randomNumbers,size);
-    str2=r.convert_to_string(randomNumbers,size);
-    str3=r.sp_char_gen(size);
+    //str2=r.convert_to_string(randomNumbers,size);
+    //str3=r.sp_char_gen(size);
+    
     string str4=str1+str3;
     string str5=str1+str2;
     string str6=str3+str2;
     string str7=str1+str2+str3;
 
-    //Password generator should have- string, number, special characters
+    //Password generator should have- string(upper as well as lower), number, special characters
     // Random string genertor
-    // Number generator- string convert
-    // Random charcters generator- string convert
+    // Number generator- A vector of numbers is created and later converted to a string 
+    // Random symbols generator- Special Symbols are created and stored in a string
+    // The String, Number and Symbols are taken and stored in a finally string from which any particular size of number is taken
     // Finally I would place them in any particular order
 
-    switch (ch)
-    {
-        case 1:
-            cout<<"\nThe Random string is: "<<str1<<endl;
-            f.create_file(str1);
-            break;
 
-        case 2:
-            cout<<"\nRandom numbers are: "<<str2<<endl;
-            f.create_file(str2);
-            break;
+    do{
+        switch (ch)
+        {
+            case 1:
+                generatedPassword=r.string_gen(size);
+                cout<<"\nThe Random string is: "<<generatedPassword<<endl;
+                if (c.ask()==true)
+                    f.create_file(generatedPassword);
+                else
+                    break;
 
-        case 3:
-            cout<<"\nRandom characters are: "<<str3<<endl;
-            f.create_file(str3);
-            break;
+            case 2:
+                generatedPassword=r.convert_to_string(randomNumbers,size);
+                cout<<"\nRandom numbers are: "<<generatedPassword<<endl;
+                if (c.ask()==true)
+                    f.create_file(generatedPassword);
+                break;
 
-        case 4:
-            cout<<"\nRandom string, charcter are: "<<endl;
-            cout<<r.final_conv(str4,size);
-            f.create_file(str4);
-            break;
+            case 3:
+                generatedPassword=r.sp_char_gen(size);
+                cout<<"\nRandom symbols are: "<<generatedPassword<<endl;
+                if (c.ask()==true)
+                    f.create_file(generatedPassword);
+                break;
 
-        case 5:
-            cout<<"\nRandom string, number are: "<<endl;
-            cout<<r.final_conv(str5,size);
-            f.create_file(str5);
-            break;
+            case 4:
+                generatedPassword=r.final_conv(str4,size);
+                cout<<"\nRandom string, symbol are: "<<generatedPassword<<endl;
+                if (c.ask()==true)
+                    f.create_file(generatedPassword);
+                break;
 
-        case 6:
-            cout<<"\nRandom charcter, number are: "<<endl;
-            cout<<r.final_conv(str6,size);
-            f.create_file(str6);
-            break;
+            case 5:
+                generatedPassword=r.final_conv(str5,size);
+                cout<<"\nRandom string, number are: "<<generatedPassword<<endl;
+                if (c.ask()==true)    
+                    f.create_file(generatedPassword);
+                break;
 
-        case 7:
-            cout<<"\nRandom string, character, number are: "<<endl;
-            cout<<r.final_conv(str7,size);
-            f.create_file(str7);
-            break;
+            case 6:
+                generatedPassword=r.final_conv(str6,size);
+                cout<<"\nRandom symbol, number are: "<<generatedPassword<<endl;
+                if (c.ask()==true)
+                    f.create_file(generatedPassword);
+                break;
 
-        default:
-            cout<<"\nThe case is not invalid"<<endl;
-            break;
-    }
+            case 7:
+                generatedPassword=r.final_conv(str7,size);
+                cout<<"\nRandom string, symbol, numbers are: "<<generatedPassword<<endl;
+                if (c.ask()==true)
+                    f.create_file(generatedPassword);
+                break;
 
+            default:
+                cout<<"\nThe case is not invalid"<<endl;
+                break;
+        }
+        
+        cout<<"\nDo you wish to continue (y/n):";
+        cin>>chc;
+    }   while(c.istrue(chc)==true);
+
+   
     return 0;
 }
